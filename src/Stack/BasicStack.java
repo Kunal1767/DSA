98b9774efabf9b35f41080704386c42efa9e14b1
kunal varshney <kunalvarshney512@gmail.com>
2024-02-19 08:15:51 +0530

    commit

--- src/Stack/BasicStack.java
+++ src/Stack/BasicStack.java
@@ -1,4 +1,17 @@
 package Stack;
 
+import java.util.Stack;
+
 public class BasicStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(2);
+        st.push(9);
+        st.push(7);
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        System.out.println(st.peek());
+    }
+
 }
